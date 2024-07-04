20645aacd2fe962690cfa45cb963950efe5a8409
Aditya Mazumdar <136506411+aditya95-pixel@users.noreply.github.com>
2024-07-05 02:49:40 +0530

    Update simple_linear_regression.R

--- Regression/Simple Linear Regression/simple_linear_regression.R
+++ Regression/Simple Linear Regression/simple_linear_regression.R
@@ -11,10 +11,6 @@ split = sample.split(dataset$Salary, SplitRatio = 2/3)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
-# Feature Scaling
-# training_set = scale(training_set)
-# test_set = scale(test_set)
-
 # Fitting Simple Linear Regression to the Training set
 regressor = lm(formula = Salary ~ YearsExperience,
                data = training_set)
